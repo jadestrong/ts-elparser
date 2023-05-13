a0d48d566b97d8b5a6ef06df165e222899718e2a
jadestrong <jadestrong@163.com>
2023-05-13 22:15:10 +0800

    Add external

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,24 @@
+import { builtinModules } from 'module'
 import swc from '@rollup/plugin-swc'
 import nodeResolve from '@rollup/plugin-node-resolve'
 import json from '@rollup/plugin-json'
 import dts from 'rollup-plugin-dts'
 import pkg from './package.json' assert { type: 'json' }
 
+const external =  Object.keys(pkg.dependencies || {})
+        .concat(Object.keys(pkg.peerDependencies || {}))
+        .concat(builtinModules);
+
+/** @type {import('rollup').RollupOptions} */
 export default [{
     input: '.build/index.js',
+    external,
     output: [
         { file: pkg.exports.import, format: 'es' },
         { file: pkg.exports.require, format: 'commonjs' },
     ],
     plugins: [
-        nodeResolve({
-            extensions: [".ts", ".js", ".json"],
-        }),
+        nodeResolve(),
         json(),
         swc({
             include: ['src/**/*.ts'],
@@ -29,5 +34,6 @@ export default [{
 }, {
     input: './.build/index.d.ts',
     output: [{ file: pkg.types, format: 'es' }],
+    external,
     plugins: [dts()]
 }]
